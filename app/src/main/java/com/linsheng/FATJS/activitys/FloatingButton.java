c4827fc05242e40e4cbe9e3ea2c40e55d0d87603
1754048656 <1754048656@qq.com>
2023-06-17 17:32:51 +0800

    悬浮窗调整

--- app/src/main/java/com/linsheng/FATJS/activitys/FloatingButton.java
+++ app/src/main/java/com/linsheng/FATJS/activitys/FloatingButton.java
@@ -56,15 +56,15 @@ public class FloatingButton extends Service {
         ViewGroup.LayoutParams txtParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         Variable.btnTextView.setText("打开");
         Variable.btnTextView.setTextSize(10);
+        Variable.btnTextView.setGravity(Gravity.CENTER); //文字居中
         Variable.btnTextView.setTextColor(Color.argb(200,10,250,0));
-        Variable.btnTextView.setPadding(5,2,5,5);
         Variable.btnTextView.setLayoutParams(txtParameters);
 
         // LinearLayout 容器
         LinearLayout.LayoutParams llParameters = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         ll.setBackgroundColor(Color.argb(180,0,0,0));
-        ll.setPadding(5, 2, 5, 5);
-        ll.setOrientation(LinearLayout.VERTICAL);
+        ll.setPadding(0, 3, 0, 0);
+        ll.setOrientation(LinearLayout.VERTICAL); //线性布局
         ll.setLayoutParams(llParameters);
 
         // 设置面板
